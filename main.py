ce659f1d6879959725e66a4d14939b177db4b6e9
mostnonattributablenameever <comfy412@protonmail.com>
2022-08-04 15:39:32 +0100

    Clean up

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from ftp_connect import ftp_fetch, ftp_push
-import CSVparsing
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     # fetch and print files from the FTP server
     print(ftp_fetch())
 
     # send files to the server
     ftp_push()  # will take file paths as arguments
-
