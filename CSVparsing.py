1a401345b9c8178accefb631fefd04b99c9efba7
FunnynameCommit <monkeydinosaur@proton.me>
2022-08-16 08:35:22 +0100

    Added the Fully Qualified validation of file data along with master validate function to check if file is Fully Qualified

--- CSVparsing.py
+++ CSVparsing.py
@@ -1,14 +1,15 @@
 import csv,re
 from tabulate import tabulate
-from datetime import datetime,timezone
+from datetime import datetime
 
 def outputNiceCsv(filename):
-    with open('output/csv/'+filename, newline='') as csvfile:
+    path='output/csv/'
+    with open(path+filename, newline='') as csvfile:
         dataReader = csv.reader(csvfile)
         print(tabulate(dataReader, headers='firstrow',tablefmt='pipe'))
 
 def validateFilename(filename):
-    '''Takes filename parameter, returns true|false for valid or invalid filename'''
+    '''Takes filename parameter, returns true|false for valid or invalid filename Returns: True|False'''
     foundData=re.search("MED_DATA_[0-9]{14}.csv",filename)
     if foundData:
         return True
@@ -21,20 +22,49 @@ def validateDateTime(fileDate):
     currentSystemDateTime="".join([datetime.now().strftime("%Y"),
 datetime.now().strftime("%m"),datetime.now().strftime("%d"),datetime.now().strftime("%H"),
 datetime.now().strftime("%M"),datetime.now().strftime("%S")])
-    if fileDate<=currentSystemDateTime:
+    if (fileDate<=currentSystemDateTime) and (len(fileDate)==len(currentSystemDateTime)):
         return True
     else:
         return False
 
 
 def validateFileData(filename):
-    '''WIP'''
-    with open('output/csv/'+filename, newline='') as csvfile:
-        dataReader = csv.reader(csvfile)
+    '''Fully Validates the files data and determines if the file is valid Returns: True|False'''
+    validFile=True
+    path='output/csv/'
+    checkListDataValues=[]
+    with open(path+filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for index,row in enumerate(reader):
+            if index==0:
+                if row[0]=="batch": # check for invalid batch_id as batch
+                    validFile=False
+                if len(row)!=12: # check for correct amount of CSV headers
+                    validFile=False
+            if index>0:
+                if row[0] in checkListDataValues: # check for duplicate batch_ids
+                    validFile=False
+                checkListDataValues.append(row[0])
+
+                for dataPoint in row[2:]: # check for if the dataPoints are invalid e.g greater than 10
+                    if float(dataPoint)>=10:
+                        validFile=False
+                    if dataPoint==None: # if dataPoint has no value assigned then file is invalid
+                        validFile=False
+    return validFile
 
 def masterValidate(filename):
-    '''here will run the master validation (composite of all other validaiton functions)'''
+    '''here will run the master validation (composite of all other validaiton functions) returns true if completely valid otherwise false'''
+    dateTimeOrder=filename.replace("MED_DATA_","")
+    dateTimeOrder=dateTimeOrder.replace(".csv","")
+    validationFuncs=[validateFilename(filename),validateDateTime(dateTimeOrder),validateFileData(filename)]
+
+    if validationFuncs[0]==True and validationFuncs[1]==True and validationFuncs[2]==True:
+        completeValid=True
+    else:
+        completeValid=False
+    return completeValid
 
 if __name__=='__main__':
-    # print(validateFilename('MED_DATA_20220605142519.csv'))
-    print(validateDateTime("20220605142519"))
+    # print(validateFileData('MED_DATA_20220605142519.csv'))
+    print(masterValidate('MED_DATA_20220605142519.csv'))
--- testCSVparsing.py
+++ testCSVparsing.py
@@ -13,7 +13,24 @@ class csvParsingValidateDateTimeTest(unittest.TestCase):
         """Should return a true/false bool"""
         self.assertIsInstance(CSVparsing.validateDateTime("05082022"), bool)
 
+    def testValidFilename(self):
+        self.assertTrue(CSVparsing.validateFilename('MED_DATA_20220605142519.csv'))
 
+    def testInvalidFilename(self):
+        self.assertFalse(CSVparsing.validateFilename('MED_DAT_20220605142519.csv'))
+
+
+class csvParsingDateTimeValidateTest(unittest.TestCase):
+
+    def testReturnType(self):
+        """Should return bool true|false"""
+        self.assertIsInstance(CSVparsing.validateDateTime("testingData"),bool)
+
+    def testValidDateTime(self):
+        self.assertTrue(CSVparsing.validateDateTime("20220804152922"))
+
+    def testInvalidDateTime(self):
+        self.assertFalse(CSVparsing.validateDateTime("11111"))
 
 if __name__=='__main__':
     unittest.main(verbosity=2)
